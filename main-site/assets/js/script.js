19c63e0148ece4f70b4272abf3886a5088257e93
VeroniqueAndPeter <veroniqueaerts2@gmail.com>
2023-02-06 14:07:32 +0100

    update

--- main-site/assets/js/script.js
+++ main-site/assets/js/script.js
@@ -17,3 +17,22 @@ function showSlides() {
     dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+var errorButton = document.getElementById("error-button");
+errorButton.addEventListener("click", function() {
+  alert("An error has occurred.");
+});
+
+
+const input1 = document.getElementById("awf_field-115331925");
+const input2 = document.getElementById("awf_field-115331926");
+const submitButton = document.getElementById("submit");
+
+input1.addEventListener("input", updateButton);
+input2.addEventListener("input", updateButton);
+
+function updateButton() {
+  submitButton.disabled = !input1.value || !input2.value;
+}
+
+updateButton();
