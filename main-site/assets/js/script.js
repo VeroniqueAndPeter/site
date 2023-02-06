b1bf9d9e96a3284891d0393014041c1e4aad486b
VeroniqueAndPeter <veroniqueaerts2@gmail.com>
2023-02-06 15:57:33 +0100

    update

--- main-site/assets/js/script.js
+++ main-site/assets/js/script.js
@@ -1,23 +1,3 @@
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
 var close = document.getElementsByClassName("closebtn");
 var i;
 
@@ -39,12 +19,11 @@ toggleBtn.addEventListener("click", function() {
 
 let targetDiv = document.getElementById("sticky-header");
 
-
-if (window.location.search.indexOf("/?sub=old") !== -1) {
+if (window.location.search.indexOf("?sub=old") !== -1) {
     targetDiv.style.display = "block";
   }
 
-  if (window.location.search.indexOf("/?sub=new") !== -1) {
+  if (window.location.search.indexOf("?sub=new") !== -1) {
     targetDiv.style.display = "block";
   }
   
