fc0d802f36ec9374affd4bf266d00fab4019d901
VeroniqueAndPeter <veroniqueaerts2@gmail.com>
2023-02-09 13:12:30 +0100

    Update product.js

--- main-site/assets/js/product.js
+++ main-site/assets/js/product.js
@@ -1,29 +1,12 @@
-
-
-// Get the product list element
-const productList = document.querySelector(".product-list");
-
-// Get the left and right buttons
-const leftBtn = document.querySelector("#left-btn");
-const rightBtn = document.querySelector("#right-btn");
-
-// Set the initial position of the product list to 0
-productList.style.left = "0";
-
-// Function to scroll the product list to the left
-function scrollLeft() {
-  productList.style.left = "-100%";
-}
-
-// Function to scroll the product list to the right
-function scrollRight() {
-  productList.style.left = "0";
-}
-
-// Attach the scrollLeft function to the left button
-leftBtn.addEventListener("click", scrollLeft);
-
-// Attach the scrollRight function to the right button
-rightBtn.addEventListener("click", scrollRight);
-
-
+document.querySelector("#left-btn").addEventListener("click", function() {
+    function scrollLeft() {
+    productList.style.left = "-100%";
+    }
+  });
+  
+  document.querySelector("#right-btn").addEventListener("click", function() {
+    function scrollRight() {
+        productList.style.left = "0";
+    }
+  });
+  
