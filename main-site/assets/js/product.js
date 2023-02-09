2af9885e975d3dc16eb95cb3dfd6ef863dfd9cdd
VeroniqueAndPeter <veroniqueaerts2@gmail.com>
2023-02-09 13:23:21 +0100

    dse

--- main-site/assets/js/product.js
+++ main-site/assets/js/product.js
@@ -1,15 +1,15 @@
-var productList = document.querySelector(".product-list");
+document.addEventListener("DOMContentLoaded", function() {
+    var productList = document.querySelector(".product-list");
 
-function scrollLeft() {
-    productList.style.left = "-100%";
-}
+    function scrollLeft() {
+        productList.style.left = "-100%";
+    }
 
-function scrollRight() {
-    productList.style.left = "0";
-}
+    function scrollRight() {
+        productList.style.left = "0";
+    }
 
-document.querySelector("#left-btn").addEventListener("click", scrollLeft);
+    document.querySelector("#left-btn").addEventListener("click", scrollLeft);
 
-document.querySelector("#right-btn").addEventListener("click", scrollRight);
-
-
+    document.querySelector("#right-btn").addEventListener("click", scrollRight);
+});
