84eba505d5f1782768aa96b542fa1d677bc73380
VeroniqueAndPeter <veroniqueaerts2@gmail.com>
2023-02-09 13:48:05 +0100

    fe

--- main-site/assets/js/product.js
+++ main-site/assets/js/product.js
@@ -2,21 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var productList = document.querySelector(".product-list");
     var leftBtn = document.querySelector("#left-btn");
     var rightBtn = document.querySelector("#right-btn");
-  
-    function scrollLeft() {
-      productList.scrollLeft -= 100;
-    }
-  
-    function scrollRight() {
-      productList.scrollLeft += 100;
-    }
-  
+    
+    leftBtn.addEventListener("click", function() {
+        productList.style.left = "0";
+    });
+    
+    rightBtn.addEventListener("click", function() {
+        productList.style.left = "-100%";
+    });
+
     leftBtn.addEventListener("click", scrollLeft);
     rightBtn.addEventListener("click", scrollRight);
-  });
-  
-  
-  
-  
-  
-  
+});
